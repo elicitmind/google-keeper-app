21d5ec39edd224fdb62524d58a903bdffa370ddd
elicitmind <elicitmind@gmail.com>
2021-07-04 21:27:03 +0200

    style css, react set up, look good

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Header() {
+    return <header><h1>Hello Friends, I am the Keeper App.</h1></header>
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./components/App.jsx"
 
-
-ReactDOM.render(<h1>Hello Friend</h1>, document.getElementById('root')
+ReactDOM.render(<App></App>, document.getElementById('root')
 );
+
+
